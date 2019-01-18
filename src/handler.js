dab397d6d3d5e5252c9011283f06b3e4133af098
Max <max@neuvians.net>
2019-01-18 11:42:56 -0500

    Added proposed worfklow

--- src/handler.js
+++ src/handler.js
@@ -6,5 +6,15 @@ export const localPayload = async () => {
 };
 
 export const handle = async event => {
+  /*
+    1. Check if repo has been scanned before
+    1a. If not scan repo for packages.json file
+    1b. If yes, check if packages.json is in in added or modified
+    2. If processing packages.json, collate dependendencies and devDependencies
+    3. Pull data from DB per dependency and repo  on previous analysis
+    4. Iterate through list and run analysis on dependency and version if it has changed
+    5. Store results in DB for repo and dependency
+    6. Open issues for suspect dependencies if they have not been flagged before  
+  */
   return event;
 };
