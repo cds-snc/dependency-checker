0f44fd1457d3377037a2fb41c2a17f1da2c241ea
Max <max@neuvians.net>
2019-01-29 13:32:01 -0500

    Firestore test

---
+++ src/__mocks__/firestore.js
@@ -0,0 +1,11 @@
+export const fixtureData = {
+  __collection__: {
+    "cds-snc-bundle-size-tracker-demo-app": {
+      __doc__: {
+        abcd: {
+          name: "rando-names"
+        }
+      }
+    }
+  }
+};
