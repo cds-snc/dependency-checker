34d2ccd110fe2db4dc2e7bd6649b7aeb04ef4ba0
Tim Arney <tim@line37.com>
2019-01-29 08:37:06 -0500

    fix test

--- src/__tests__/lib/queryNpms.test.js
+++ src/__tests__/lib/queryNpms.test.js
@@ -1,11 +1,7 @@
 import { fetch } from "node-fetch";
 import { queryNpms } from "../../lib/queryNpms";
 
-// @todo this needs to be fixed
-/*
 jest.mock("node-fetch", () => {
-  
-  
   const actualFetch = require.requireActual("node-fetch");
   return {
     ...actualFetch,
@@ -20,11 +16,9 @@ jest.mock("node-fetch", () => {
     })
   };
 });
-*/
 
 test("queries Npms for data about a package", async () => {
-  const result = await queryNpms("foo");
-  // expect(fetch).toHaveBeenCalledTimes(1);
-  // expect(result).toEqual({ score: { final: 1 } });
-  expect(true).toEqual(true);
+  const result = await queryNpms("foo", fetch);
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(result).toEqual({ score: { final: 1 } });
 });
--- src/lib/queryNpms.js
+++ src/lib/queryNpms.js
@@ -1,6 +1,6 @@
-import {default as fetch} from "node-fetch";
+import { default as defaultFetch } from "node-fetch"; // eslint-disable-line import/no-named-default
 
-export const queryNpms = async name => {
+export const queryNpms = async (name, fetch = defaultFetch) => {
   const path = `https://api.npms.io/v2/package/${encodeURIComponent(name)}`;
   let response = await fetch(path);
   const result = await response.json();
